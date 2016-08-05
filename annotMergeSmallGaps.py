72d22cd03cc6043c7ea22fe474a19f7707f5f100
Bruno Souza <fsouza.bruno@usp.br>
2016-08-05 11:14:06 -0300

    separated merge from parsing; added a -v arg to annotParser

---
+++ annotMergeSmallGaps.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse as argp
+import os
+import sys
+
+import numpy as np
+
+from extb.utils import magic_open
+from extb import parse, input_formats, output_formats
+
+
+def main():
+    ap = argp.ArgumentParser(description="merge small gaps from annotation files")
+    ap.add_argument('-i', '--input',
+                    help="input file. to read from pipe, use the argument 'stdin'")
+    ap.add_argument('-o', '--output',
+                    help='output file', default=sys.stdout)
+    ap.add_argument('-f', '--input_format',
+                    help='input file format (supported formats: %s)' % ', '.join(input_formats),
+                    default='bed')
+    ap.add_argument('-t', '--output_format',
+                    help='output file format (supported formats: %s)' % ', '.join(output_formats),
+                    default='bed')
+    ap.add_argument('-s', '--small_gap_size', type=int, default=9)
+
+
+    args = ap.parse_args()
+
+    if args.input_format:
+        input_format = args.input_format
+        if input_format not in input_formats:
+            raise SystemExit('ERROR: %s extension is not supported' % input_format)
+
+    if args.output_format:
+        output_format = args.output_format
+        if output_format not in output_formats:
+            raise SystemExit('ERROR: %s extension is not supported' % output_format)
+
+    if args.input:
+        if args.input == 'stdin':
+            f_in = sys.stdin
+        elif not os.path.exists(args.input):
+            raise SystemExit("ERROR: input file %s doesn't exist" % args.input)
+        else:
+            f_in = magic_open(args.input)
+    else:
+        # ap.print_help()
+        ap.print_usage()
+        quit()
+
+    if args.output:
+        if args.output == sys.stdout:
+            f_out = sys.stdout
+
+        elif os.path.exists(args.output):
+            raise SystemExit('ERROR: %s already exists!!!' % args.output)
+
+        else:
+            # dirname = os.path.dirname(args.output)
+            # os.makedirs(dirname, exist_ok=True)
+            # print('created dir', dirname, file=sys.stderr)
+            f_out = open(args.output, 'w')
+
+    for annotation in parse(f_in, input_format):
+        if annotation.blockCount() > 1:
+            small_gap = args.small_gap_size
+            annotation = annotation.merge_small_gap(small_gap)
+
+        # try:
+        print(annotation.format(output_format), file=f_out, sep='\t')
+        # except IndexError:
+        #     print(annotation)
+
+    f_in.close()
+    f_out.close()
+
+if __name__ == '__main__':
+    # Deal with broken pipe error (errno 32)
+    # http://stackoverflow.com/a/16865106
+    from signal import signal, SIGPIPE, SIG_DFL, SIGINT
+
+    # deal with broken pipe
+    signal(SIGPIPE, SIG_DFL)
+    # deal with KeyboardInterrupt
+    signal(SIGINT, SIG_DFL)
+
+    main()
--- annotParser.py
+++ annotParser.py
@@ -22,72 +22,95 @@ def main():
     ap.add_argument('-t', '--output_format',
                     help='output file format (supported formats: %s)' % ', '.join(output_formats),
                     default='bed')
-    ap.add_argument('-mg', '--merge_small_gaps', type=int, default=False)
+    ap.add_argument('-n', '--min_exon_count', type=int, default=1,
+                    help='min number of exons (inclusive)')
+
     ap.add_argument('-igs', '--ignore_gaps_smaller_than', type=int, default=False)
     ap.add_argument('-igb', '--ignore_gaps_bigger_than', type=int, default=False)
 
-    args = ap.parse_args()
+    ap.add_argument('-v', '--invert_match', default=False, action='store_true',
+                    help='only return false results (similar to grep -v)',)
 
-    if args.input_format:
-        input_format = args.input_format
+    argv = ap.parse_args()
+
+    if argv.input_format:
+        input_format = argv.input_format
         if input_format not in input_formats:
             raise SystemExit('ERROR: %s extension is not supported' % input_format)
 
-    if args.output_format:
-        output_format = args.output_format
+    if argv.output_format:
+        output_format = argv.output_format
         if output_format not in output_formats:
             raise SystemExit('ERROR: %s extension is not supported' % output_format)
 
-    if args.input:
-        if args.input == 'stdin':
+    if argv.input:
+        if argv.input == 'stdin':
             f_in = sys.stdin
-        elif not os.path.exists(args.input):
-            raise SystemExit("ERROR: input file %s doesn't exist" % args.input)
+        elif not os.path.exists(argv.input):
+            raise SystemExit("ERROR: input file %s doesn't exist" % argv.input)
         else:
-            f_in = magic_open(args.input)
+            f_in = magic_open(argv.input)
     else:
         # ap.print_help()
         ap.print_usage()
         quit()
 
-    if args.output:
-        if args.output == sys.stdout:
+    if argv.output:
+        if argv.output == sys.stdout:
             f_out = sys.stdout
 
-        elif os.path.exists(args.output):
-            raise SystemExit('ERROR: %s already exists!!!' % args.output)
+        elif os.path.exists(argv.output):
+            raise SystemExit('ERROR: %s already exists!!!' % argv.output)
 
         else:
-            # dirname = os.path.dirname(args.output)
+            # dirname = os.path.dirname(argv.output)
             # os.makedirs(dirname, exist_ok=True)
             # print('created dir', dirname, file=sys.stderr)
-            f_out = open(args.output, 'w')
+            f_out = open(argv.output, 'w')
 
-    for annotation in parse(f_in, input_format):
-        if annotation.blockCount() > 1:
-            if args.merge_small_gaps:
-                small_gap = args.merge_small_gaps
-                annotation = annotation.merge_small_gap(small_gap)
+    def filter(annotation, min_exon_count, ignore_small_gaps, ignore_big_gaps):
+        c = 0
 
+        if annotation.blockCount() < min_exon_count:
+            # if we already know the count is above the minimum, just ignore it
+            return False
+
+        elif annotation.blockCount() > 1:
             # ignore small introns
-            if args.ignore_gaps_smaller_than:
-                small_gap = args.ignore_gaps_smaller_than
+            if ignore_small_gaps:
+                small_gap = ignore_small_gaps
                 gaps = annotation.introns
-                small_gaps_count = np.sum(gaps <= small_gap)
+                small_gaps_count = np.sum(gaps < small_gap)
                 if small_gaps_count > 0:
-                    continue
+                    c += 1
 
             # ignore HUGE introns
-            if args.ignore_gaps_bigger_than:
-                huge_gap = args.ignore_gaps_bigger_than
+            if ignore_big_gaps:
+                huge_gap = ignore_big_gaps
                 gaps = annotation.introns
                 huge_gaps_count = np.sum(gaps > huge_gap)
                 if huge_gaps_count > 0:
-                    continue
-        # try:
-        print(annotation.format(output_format), file=f_out, sep='\t')
-        # except IndexError:
-        #     print(annotation)
+                    c += 1
+
+        if c > 0:
+            return False
+
+    # list of args to be used on filter function
+    args = [argv.min_exon_count,
+            argv.ignore_gaps_smaller_than,
+            argv.ignore_gaps_bigger_than]
+
+    # argv -v / --invert_match
+    match = True
+    if argv.invert_match:
+        match = False
+
+
+    for annotation in parse(f_in, input_format):
+
+        if filter(annotation, *args) == match:
+            print(annotation.format(output_format), file=f_out, sep='\t')
+
 
     f_in.close()
     f_out.close()
